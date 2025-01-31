bd8dca2d1030b2a0975166c5afc0f6b5f76f0985
Yogesh Sharma <yogesh.sharma12041@gmail.com>
2025-01-31 11:00:21 +0530

    Update manifest.json

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    name: "LinkedIn Auto Connector",
+    description:
+      "it is a chrome extesnion that helps user to connect and follow the people on linkedIn",
+    version: "0.1.0",
+  },
 });
